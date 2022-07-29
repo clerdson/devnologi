8502887fb3ac5907400a983d70aa0c038f9fb0d8
clerdson <clerdson@gmail.com>
2022-07-28 21:15:36 -0300

    images

--- lib/features/next/widgets/horizontal_avatars.dart
+++ lib/features/next/widgets/horizontal_avatars.dart
@@ -13,20 +13,52 @@ class HStack extends StatefulWidget {
 class _HStackState extends State<HStack> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (ctx, i) {
-          return CircleAvatar(
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+ CircleAvatar(
               radius: 40.0,
               backgroundColor: Colors.transparent,
               child: Column(
                 children: [
-                  Image.asset('avatar.png'),
+                  Image.asset('Group1.png'),
                   const SizedBox(height: 10,),
                   const Text('Shoes',style: TextStyle(color: Colors.black),),
                 ],
-              ));
-        });
+              )),
+              CircleAvatar(
+              radius: 40.0,
+              backgroundColor: Colors.transparent,
+              child: Column(
+                children: [
+                  Image.asset('Group3.png'),
+                  const SizedBox(height: 10,),
+                  const Text('Shoes',style: TextStyle(color: Colors.black),),
+                ],
+              )),
+              CircleAvatar(
+              radius: 40.0,
+              backgroundColor: Colors.transparent,
+              child: Column(
+                children: [
+                  Image.asset('Group2.png'),
+                  const SizedBox(height: 10,),
+                  const Text('Shoes',style: TextStyle(color: Colors.black),),
+                ],
+              )),
+              CircleAvatar(
+              radius: 40.0,
+              backgroundColor: Colors.transparent,
+              child: Column(
+                children: [
+                  Image.asset('Group4.png'),
+                  const SizedBox(height: 10,),
+                  const Text('Shoes',style: TextStyle(color: Colors.black),),
+                ],
+              ))
+      ],
+       
+    ); 
+      
   }
 }
